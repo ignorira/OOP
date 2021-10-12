eb47b8d15edcdaa71df89c99de9f8c8b361db1df
ignorira <58992611+ignorira@users.noreply.github.com>
2021-10-12 22:19:46 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,16 +6,6 @@ namespace RaceSimulator
     class Program
     {
         static void Main(string[] args)
-        {
-            var race = new MixedRace(1000);
-            race.Start();
-            var idx = 1;
-            foreach (var result in race.Results)
-            {
-                Console.WriteLine($"{idx++}: {result.Vehicle.Name} - {result.Time}");
-            }
-            Console.WriteLine($"Winner: {race.WinnerResult.Vehicle.Name}");
-            Console.WriteLine();
-        }
+        {}
     }
 }
